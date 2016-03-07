77686472268909c078317404cb3294c5ab117ff6
Ömer Faruk Demir <ofaruk.demir@gmail.com>
2016-03-07 10:49:43 +0200

    redefinition

---
+++ TangentIdeas.Web/Bootstrapper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using System.Web;
+using System.Web.Hosting;
+using Nancy;
+using HttpUtility = Nancy.Helpers.HttpUtility;
+
+namespace TangentIdeas.Web {
+    public class Bootstrapper : DefaultNancyBootstrapper {
+        // The bootstrapper enables you to reconfigure the composition of the framework,
+        // by overriding the various methods and properties.
+        // For more information https://github.com/NancyFx/Nancy/wiki/Bootstrapper
+
+        private byte[] _favicon;
+
+        protected override byte[] FavIcon => _favicon ?? (this._favicon = LoadFavIcon());
+
+        private byte[] LoadFavIcon() {
+            var favIcon = File.ReadAllBytes(HttpContext.Current.Server.MapPath("favicon.ico"));
+            return favIcon;
+        }
+    }
+}
---
+++ TangentIdeas.Web/Modules/IndexModule.cs
@@ -0,0 +1,23 @@
+﻿using Nancy;
+using Nancy.ModelBinding;
+
+namespace TangentIdeas.Web.Modules {
+    public class IndexModule : NancyModule {
+        public IndexModule() {
+            Get["/"] = parameters => View["index"];
+
+            Post["/contact"] = parameters => {
+                var data = this.Bind<ContactModel>();
+                //TODO: mail gönderimi olacak
+                return View["index"];
+            };
+        }
+    }
+
+    public class ContactModel {
+        public string Fullname { get; set; }
+        public string Email { get; set; }
+        public string Message { get; set; }
+        public string Reason { get; set; }
+    }
+}
